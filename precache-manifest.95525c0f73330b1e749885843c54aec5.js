10d1347dbfea1297a09b78c8b0f7d6a03c752d80
Scenaristeur <scenrister@galcm>
2021-05-18 01:49:18 +0200

    add socket.io

---
+++ precache-manifest.95525c0f73330b1e749885843c54aec5.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e8f85c839473dd2652a",
+    "url": "/vatch-vue/css/app.45f39740.css"
+  },
+  {
+    "revision": "298054a066e678a7c823",
+    "url": "/vatch-vue/css/chunk-vendors.25ab3afa.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vatch-vue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "ba91006902d78bc78e558a22dcceb791",
+    "url": "/vatch-vue/index.html"
+  },
+  {
+    "revision": "69d08851925f435db738",
+    "url": "/vatch-vue/js/about.092524fa.js"
+  },
+  {
+    "revision": "2e8f85c839473dd2652a",
+    "url": "/vatch-vue/js/app.7b81f6c8.js"
+  },
+  {
+    "revision": "afc8e3668a2a0d837eca",
+    "url": "/vatch-vue/js/chunk-2c2e613c.f00ddd0e.js"
+  },
+  {
+    "revision": "e7e6e5d4aa0536cb7fa0",
+    "url": "/vatch-vue/js/chunk-2d0c7cf2.32e1ce89.js"
+  },
+  {
+    "revision": "ee0b6d8f2bf5e8577345",
+    "url": "/vatch-vue/js/chunk-2d0f0d96.1fb430ce.js"
+  },
+  {
+    "revision": "102bd80b8c03c53b0918",
+    "url": "/vatch-vue/js/chunk-da0a3822.125db82e.js"
+  },
+  {
+    "revision": "298054a066e678a7c823",
+    "url": "/vatch-vue/js/chunk-vendors.fca5ca74.js"
+  },
+  {
+    "revision": "2f352f058186cf23f2afbee1b0a91dc1",
+    "url": "/vatch-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vatch-vue/robots.txt"
+  }
+]);
