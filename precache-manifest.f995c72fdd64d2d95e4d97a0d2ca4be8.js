99e6c0c45cfa980599c82792b7814caeb5c69fda
Scenaristeur <scenaristeur@gmail.com>
2021-05-22 14:05:27 +0200

    tag things of a resource with wikidata

---
+++ precache-manifest.f995c72fdd64d2d95e4d97a0d2ca4be8.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d643b5eedf8ff4b83250",
+    "url": "/vatch-vue/css/app.46514082.css"
+  },
+  {
+    "revision": "639dae623832108183e6",
+    "url": "/vatch-vue/css/chunk-11b6ca05.d94dc95c.css"
+  },
+  {
+    "revision": "1c39c38a6048b7c5b749",
+    "url": "/vatch-vue/css/chunk-49693460.7d88351a.css"
+  },
+  {
+    "revision": "7201a42b7b2c9e4b9bc4",
+    "url": "/vatch-vue/css/chunk-vendors.1d5dfc9b.css"
+  },
+  {
+    "revision": "73fbd08ceaa56085fd5b843d8745cf00",
+    "url": "/vatch-vue/index.html"
+  },
+  {
+    "revision": "c97a99edbfa4a495856e",
+    "url": "/vatch-vue/js/about.f17f84a0.js"
+  },
+  {
+    "revision": "d643b5eedf8ff4b83250",
+    "url": "/vatch-vue/js/app.3f469c1b.js"
+  },
+  {
+    "revision": "639dae623832108183e6",
+    "url": "/vatch-vue/js/chunk-11b6ca05.4a770926.js"
+  },
+  {
+    "revision": "afc8e3668a2a0d837eca",
+    "url": "/vatch-vue/js/chunk-2c2e613c.f00ddd0e.js"
+  },
+  {
+    "revision": "059791c9ef04c85fbf7e",
+    "url": "/vatch-vue/js/chunk-2d0a4638.20f52174.js"
+  },
+  {
+    "revision": "4b66135e78cb2c730b14",
+    "url": "/vatch-vue/js/chunk-2d0ac44b.6cc55334.js"
+  },
+  {
+    "revision": "6721c84c80788c2ff30d",
+    "url": "/vatch-vue/js/chunk-2d0bceed.cd8f87ad.js"
+  },
+  {
+    "revision": "cea30747af3c17c169ba",
+    "url": "/vatch-vue/js/chunk-2d0c7cf2.dfae5d7e.js"
+  },
+  {
+    "revision": "04e1868f22bc41430947",
+    "url": "/vatch-vue/js/chunk-2d0f0d96.878cbb7e.js"
+  },
+  {
+    "revision": "ab411949bada1a9e9d7c",
+    "url": "/vatch-vue/js/chunk-2d207e90.47011d2c.js"
+  },
+  {
+    "revision": "92e776cf79f1d33c5f36",
+    "url": "/vatch-vue/js/chunk-2d222392.d2091b1a.js"
+  },
+  {
+    "revision": "1c39c38a6048b7c5b749",
+    "url": "/vatch-vue/js/chunk-49693460.758c0dc8.js"
+  },
+  {
+    "revision": "7918cca18d2a11a16b6c",
+    "url": "/vatch-vue/js/chunk-702a9089.30a7ecc7.js"
+  },
+  {
+    "revision": "9e5896920659e159d391",
+    "url": "/vatch-vue/js/chunk-74a2efbf.f51828ee.js"
+  },
+  {
+    "revision": "db1c5c6004d96b886549",
+    "url": "/vatch-vue/js/chunk-d8fe2374.5bc90e43.js"
+  },
+  {
+    "revision": "7201a42b7b2c9e4b9bc4",
+    "url": "/vatch-vue/js/chunk-vendors.ae22a945.js"
+  },
+  {
+    "revision": "2f352f058186cf23f2afbee1b0a91dc1",
+    "url": "/vatch-vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vatch-vue/robots.txt"
+  }
+]);
